f82c08938abf9561554334f1f911104879e29e4f
Fpantoja2001 <121701118+Fpantoja2001@users.noreply.github.com>
2023-01-12 16:53:41 -0500

    landing page skeleton complete

--- portfolio/src/components/Main/main.jsx
+++ portfolio/src/components/Main/main.jsx
@@ -4,7 +4,7 @@ import ImageSlider from './slider'
 
 function Main(){
     const slides = [
-        {url:"https://upload.wikimedia.org/wikipedia/commons/e/e5/Redsquare.png"},
+        { url:"https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"},
         {url:"https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"},
         {url:"https://images.pexels.com/photos/235621/pexels-photo-235621.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"}
     ]
--- portfolio/src/components/Main/slider.jsx
+++ portfolio/src/components/Main/slider.jsx
@@ -2,7 +2,19 @@ import { useState } from "react";
 import './slider.scss'
 
 const ImageSlider = ({slides}) => {
-    const [currentIndex,setCurrentUser] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const backFunction = () => {
+        const isFirstSlide = currentIndex == 0;
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1
+        setCurrentIndex(newIndex)
+    }
+
+    const nextFunction = () => {
+        const isLastSlide = currentIndex == slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex)
+    }
 
     return (
         <div className="slideContainer">
@@ -15,10 +27,17 @@ const ImageSlider = ({slides}) => {
             <span className="s_over"></span>
             <span className="s_over"></span>
             <span className="s_over"></span>
+            
+      
             <div className='slideComponent'>
+                <img style={{ backgroundImage: `url(${slides[currentIndex].url})` }}></img>
                 <div className="slideComponentText">Tilt Effect</div>
-                <span class="material-symbols-outlined" >arrow_forward_ios</span>
-                <div className="projectDescript"></div>
+                <span class="material-symbols-outlined" id='frontArrow' onClick={nextFunction}>arrow_forward_ios</span>
+                <span class="material-symbols-outlined" id='backArrow' onClick={backFunction}>arrow_back_ios</span>
+                <div className="projectDescript">
+                    <h1>Projet title</h1>
+                    <p>Lorem ipsum</p>
+                </div>
             </div>
         </div>
     )
